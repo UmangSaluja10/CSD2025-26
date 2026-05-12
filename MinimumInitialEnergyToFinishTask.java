10bab251bdf40fd76b4ff87e9d2293c2362b2928
UmangSaluja10 <umangsaluja99@gmail.com>
2026-05-12 23:40:27 +0530

    52nd commit

---
+++ MinimumInitialEnergyToFinishTask.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int minimumEffort(int[][] tasks) {
+        Arrays.sort(tasks, (a, b) -> {
+            return (b[1] - b[0]) - (a[1] - a[0]);
+        });
+
+        int initialEnergy = 0;
+        int currentEnergy = 0;
+
+        for (int[] task : tasks) {
+            int actual = task[0];
+            int minimum = task[1];
+
+            if (currentEnergy < minimum) {
+                initialEnergy += minimum - currentEnergy;
+                currentEnergy = minimum;
+            }
+
+            currentEnergy -= actual;
+        }
+
+        return initialEnergy;
+    }
+}
