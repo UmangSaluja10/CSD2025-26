28d57b5f31e19281a6437575f0f1508511e4701a
UmangSaluja10 <umangsaluja99@gmail.com>
2026-06-14 01:34:22 +0530

    82nd commit

---
+++ JumpGame.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int farthest = 0;
+        for(int i=0;i<nums.length;i++){
+            if(i<=farthest){
+                farthest = Math.max(farthest,i+nums[i]);
+            }else return false;
+            if(farthest >= nums.length - 1)return true;
+        }
+        return true;
+    }
+}
